e79a5eea51b73815df12bcd4421aad9fd7c16df3
Małgorzata Molenda <malgorzata.molenda@currenda.pl>
2019-06-15 10:20:39 +0200

    zadanie 2

---
+++ GuessTheMovie/src/GuessTheMovie.java
@@ -0,0 +1,67 @@
+/*
+
+ */
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+
+public class GuessTheMovie  {
+
+    public static void main(String[] args)throws Exception{
+        File file = new File("movies.txt");
+        Scanner scanner = new Scanner(file);
+
+        // do tablicy wrzucamy wszystkie tytuły z pliku movies.txt
+        ArrayList<String> movies;
+        movies = new ArrayList<>();
+
+        while (scanner.hasNextLine()) {
+            movies.add(scanner.nextLine());
+        }
+
+        // losujemy jeden tytul od 1 do 25 = movies.size()
+        int randomNumber = (int) (Math.random()*movies.size()) ;
+        String randomMovies = movies.get(randomNumber);
+        String rightLetters ="";
+        String wrongLetters ="";
+        int countLost=0;
+        boolean gameWon = false;
+
+
+        //jest 10 prób niezgadniecia litery
+        while(countLost<10){
+            //wyświetlamy podkreślenia "_" w miejsce prawdziwych liter tym samym podając liczbę liter
+            if ( rightLetters.equals("") ) {
+                System.out.println("Zgadujesz: "+randomMovies.replaceAll("[a-zA-Z]","_"));
+            }
+            else  {
+                System.out.println("Zgadujesz: "+randomMovies.replaceAll("[a-zA-Z&&[^" + rightLetters + "]]" , "_"));
+            }
+            System.out.println("Zgadnij literę: ");
+            Scanner scaner = new Scanner(System.in);
+            String letter = scaner.next().toLowerCase();
+            if (randomMovies.toLowerCase().contains(letter)){
+                rightLetters =rightLetters.concat(letter).concat(letter.toUpperCase());
+            }
+            else{
+                countLost++;
+                wrongLetters = wrongLetters.concat(" ").concat(letter);
+            }
+            System.out.println("Zgadłeś ("+countLost+") litery. Twoje złe litery:"+wrongLetters);
+
+            // sprawdzamy czy wygrana
+            if (rightLetters.length()>0) {
+
+                if (!randomMovies.replaceAll("[a-zA-Z&&[^" + rightLetters + "]]", "_").contains("_")) {
+                    System.out.println("Wygrałes!");
+                    System.out.println("Odgadnięty tytuł: "+randomMovies);
+                    return;
+                }
+            }
+        }
+        System.out.println(("Przegrałeś!"));
+    }
+
+}
