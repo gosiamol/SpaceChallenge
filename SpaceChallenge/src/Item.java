e985eaf876cb8c46c763e3b8b3f4af717d5ae4a5
Małgorzata Molenda <malgorzata.molenda@currenda.pl>
2019-06-15 15:08:02 +0200

    class SpaceShip ini

--- SpaceChallenge/src/Item.java
+++ SpaceChallenge/src/Item.java
@@ -1,7 +1,3 @@
-/*Create an Item class that includes a
-String name and an
-int weight that will represent an item to be carried by the rockets
-*/
 public class Item {
     private String name;
     private int weight;
